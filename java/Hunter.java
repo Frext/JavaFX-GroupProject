f2c441530817e582c8fc3c3ca9994e71e2ccabb0
Furkan Usul <furkanusul28@gmail.com>
2026-04-16 15:11:34 +0300

    Implement static triangle

--- java/Hunter.java
+++ java/Hunter.java
@@ -3,6 +3,8 @@ package com.group1.groupproject;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Rotate;
 
 public class Hunter extends Entity {
     public Group implementView(){
@@ -10,9 +12,22 @@ public class Hunter extends Entity {
         Circle circle = new Circle(15);
         circle.setFill(Color.ORANGE);
 
-        // TODO: implement triangle
+        Polygon triangle = new Polygon();
 
-        group.getChildren().addAll(circle);
+        // Triangle points right which is 0 degree
+        triangle.getPoints().addAll(0.0,0.0, -100.0, -30.0, -100.0, 30.0);
+        triangle.setFill(Color.RED);
+        triangle.setOpacity(.5);
+
+        group.getChildren().addAll(triangle, circle);
+
+        group.parentProperty().addListener((observable,  oldValue, parent) -> {
+            if (parent != null){
+                parent.setOnMouseMoved(e->  {
+                    // TODO: Change rotation for triangle
+                });
+            }
+        });
 
         return group;
     }
